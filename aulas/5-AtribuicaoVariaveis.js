df47a01d58f9d7da8844a7d78cf12c1ac83daa99
guileite97 <guileite97@hotmail.com>
2021-04-24 18:39:10 -0300

    js-5-6

---
+++ aulas/5-AtribuicaoVariaveis.js
@@ -0,0 +1,19 @@
+console.log("Trabalhando com variáveis")
+
+
+let nome = "Guilherme"
+const sobrenome = "Leite"
+
+console.log(nome, sobrenome)
+
+console.log(`Meu nome é ${nome} ${sobrenome}`)
+
+const nomeCompleto = nome + sobrenome
+
+console.log(nomeCompleto)
+
+
+let idade
+idade = 23
+idade = idade + 1
+console.log(idade)

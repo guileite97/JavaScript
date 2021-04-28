4fd3e365832b1df52dcb185c1f784a577abddb92
guileite97 <guileite97@hotmail.com>
2021-04-27 21:35:52 -0300

    js-web2

--- aulas/javascript web/introducao-javascript/js/principal.js
+++ aulas/javascript web/introducao-javascript/js/principal.js
@@ -1,4 +1,33 @@
+var pesoValido = true
+var alturaValido = true
+
 var titulo = document.querySelector(".titulo")
-			
 titulo.textContent = "Aparecida Nutricionista"
+
+var paciente = document.querySelector("#primeiro-paciente")
+var tdPeso = paciente.querySelector(".info-peso")
+var peso = tdPeso.textContent
+
+
+var tdAltura = document.querySelector(".info-altura")
+var altura = tdAltura.textContent
+
+var titulo1 = document.querySelector("#primeiro-paciente")
+var pImc = titulo1.querySelector(".info-imc")
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso invalido")
+    pesoValido = false
+    pImc.textContent = "Peso invalido!"
+}
+if(altura <= 0 || altura >= 3.00){
+    console.log("Altura invalido")
+    alturaValido = false
+    pImc.textContent = "Altura invalido!"
+}
+if(pesoValido && alturaValido){
+    var imc = peso / (altura * altura)
+    pImc.textContent = imc
+}
+
 
---
+++ aulas/javascript web/prato-do-dia/js/principal.js
@@ -0,0 +1,21 @@
+
+var pratoDoDia = document.querySelector("#sexta");
+var salada = pratoDoDia.querySelector(".salada").textContent;
+var principal = pratoDoDia.querySelector(".principal").textContent;
+var acompanhamento = pratoDoDia.querySelector(".acompanhamento").textContent;
+
+console.log("Prato do dia!");
+console.log(salada);
+console.log(principal);
+console.log(acompanhamento);
+
+
+var pratoDoDia2 = document.querySelector("#segunda");
+var salada2= pratoDoDia2.querySelector(".salada").textContent
+var principal2= pratoDoDia2.querySelector(".principal").textContent
+var acompanhamento2= pratoDoDia2.querySelector(".acompanhamento").textContent
+
+console.log("\nPrato da segunda!");
+console.log(salada2);
+console.log(principal2);
+console.log(acompanhamento2);

6a1ec57724fea6b676f1add1343a2c36cc7dd222
guileite97 <guileite97@hotmail.com>
2021-05-05 21:15:26 -0300

    dom

---
+++ aulas/javascript web/dom/js/script.js
@@ -0,0 +1,13 @@
+var p1 = document.getElementsByTagName("p")[1]
+p1.style.color = "blue"
+var corpo = document.body
+corpo.style.background = "black"
+
+/*var d = document.getElementById('msg')
+
+var d = document.getElementsByName('msg')[0]
+d.innerText = "Olá"*/
+
+var d = document.querySelector("div#msg")
+d.style.background = 'blue'
+
